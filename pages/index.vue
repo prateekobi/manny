f220280beb82bf0db5ed212264a9c951c0531ac0
prateekobi <prateekobi@gmail.com>
2021-07-03 03:49:10 +1000

    Added stars

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,14 @@
     <div
       class="container-fluid page-wrapper px-0 d-flex justify-content-center"
     >
+      <div v-if="is_night">
+        <div id="stars-group-1"></div>
+        <div id="stars-group-2"></div>
+        <div id="stars-group-3"></div>
+        <div id="stars-group-4"></div>
+        <div id="stars-group-5"></div>
+        <div id="stars-group-6"></div>
+      </div>
       <div class="container align-self-center">
         <div class="row mx-n1">
           <div class="col-12 px-1 text-center">
@@ -56,7 +64,7 @@ export default {
 .wrapper {
   background-color: #cd113b;
   &.is-night {
-    background-color: #171717;
+    background-color: #000;
     .title {
       color: #185adb;
       font-size: 20px;
@@ -74,6 +82,76 @@ export default {
 
   .page-wrapper {
     height: 100vh;
+  }
+}
+
+@function generate-random-stars($screen-width, $color, $density) {
+  $h-shadow: random($screen-width);
+  $v-shadow: random($screen-width);
+
+  $stars: "#{$h-shadow}px #{$v-shadow}px #{$color}";
+  @for $i from 2 through $density {
+    $h-shadow: random($screen-width);
+    $v-shadow: random($screen-width);
+
+    $stars: "#{$stars}, #{$h-shadow}px #{$v-shadow}px #{$color}";
+  }
+  @return unquote($stars);
+}
+
+@mixin stars-content($screen-width, $color, $density, $speed, $delay) {
+  width: 2px;
+  height: 2px;
+  border-radius: 50%;
+  opacity: 0;
+  box-shadow: generate-random-stars($screen-width, $color, $density);
+  animation-name: glowing-stars;
+  animation-duration: $speed + s;
+  animation-iteration-count: infinite;
+  animation-direction: alternate;
+  animation-timing-function: linear;
+  animation-delay: $delay + s;
+}
+
+body {
+  background: #000;
+}
+
+//Change colors for multi-colors star groups
+
+#stars-group-1 {
+  @include stars-content(1800, #fff, 200, 1, 0);
+}
+
+#stars-group-2 {
+  @include stars-content(1800, #fff, 200, 1, 0.1);
+}
+
+#stars-group-3 {
+  @include stars-content(1800, #fff, 200, 1, 0.2);
+}
+
+#stars-group-4 {
+  @include stars-content(1800, #fff, 200, 1, 0.3);
+}
+
+#stars-group-5 {
+  @include stars-content(1800, #fff, 200, 1, 0.4);
+}
+
+#stars-group-6 {
+  @include stars-content(1800, #fff, 200, 1, 0.5);
+}
+
+@keyframes glowing-stars {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
   }
 }
 </style>
